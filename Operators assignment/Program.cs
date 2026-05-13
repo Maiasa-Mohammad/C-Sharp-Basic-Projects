bfba343dfbfb7aafcf86e14d4e0da70c7b0ac4b0
Maiasa-Mohammad <Maiasa.Mohammad@outlook.com>
2026-05-13 12:00:55 +0300

    Update Program.cs

--- Operators assignment/Program.cs
+++ Operators assignment/Program.cs
@@ -59,8 +59,8 @@ class Program
         Employee emp1 = new Employee
         {
             Id = 101,
-            FirstName = "Kawthar",
-            LastName = "AlHallak"
+            FirstName = "Maisa",
+            LastName = "Mohammad"
         };
 
         // 🏗️ Creating the second employee — same Id to test equality logic
@@ -68,7 +68,7 @@ class Program
         {
             Id = 101,
             FirstName = "Alicia",
-            LastName = "Johns"
+            LastName = "Keys"
         };
 
         // 🧪 Testing equality using our custom overloaded operator
